156f241a0d982d58255041c49f09275b07ce8886
MBP <mbp@MBPs-MacBook-Pro.local>
2019-01-06 12:17:32 -0500

    Location should be a list

--- specs.hs
+++ specs.hs
@@ -1,8 +1,7 @@
 import Data.GPS as G
 import Crypto.Hash
 
-data StartLocation = Maybe G.Location
-data EndLocation = Maybe G.Location
+data Location = G.Location
 data JobOfferHash = Digest SHA256
 data JobHash = Digest SHA256
 data UserHash = Digest SHA256
@@ -10,21 +9,21 @@ data EmployerPublicKey = Digest SHA256
 data EmployeePublicKey = Digest SHA256
 data NumberOfPositions = Int
 
--- These lines that look like type signatures, are just a pseudo-code
--- expression of some basic functions which could be part of the system.
+-- These lines that look like type signatures, are just pseudo-code.
+-- They try to express some basic functions which could be part of the system.
 -- These aren't intended to be viewed as actual types where the real world effects of the function are encoded.
 
-createJobOffer :: EmployerPublicKey -> StartLocation -> EndLocation -> PaymentAmount -> JobOfferHash
+createJobOffer :: EmployerPublicKey -> [Location] -> PaymentAmount -> JobOfferHash
 
 applyForJob :: EmployeePublicKey -> JobOfferHash -> Signature -> ApplicationHash
 
 acceptJobApplication :: EmployerPublicKey -> ApplicationHash -> JobHash
 
-checkIn :: JobHash -> G.Location -> CheckInHash
+checkIn :: JobHash -> Location -> CheckInHash
 
-checkOut :: JobHash -> G.Location -> CheckOutHash
+checkOut :: JobHash -> Location -> CheckOutHash
 
--- There is a relational
+
 
 -- Questions
 
